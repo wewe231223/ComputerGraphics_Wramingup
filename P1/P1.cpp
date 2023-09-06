88d27cb24e80d9c28ff50fa717d051fa16f230d1
승범 김 <wewe231223@kpu.ac.kr>
2023-09-07 01:26:32 +0900

    컴퓨터 그래픽스 워밍업  1주차 #1
    Matrix 클래스의
    todo 참고

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -1,20 +1,347 @@
 ﻿// P1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
+/*
+2차원 순회 
+
+
+
+        for (auto i = 0; i < this->size; ++i) {
+            for (auto j = 0; j < this->size; ++j) {
+
+
+            }
+        }
+
+
+*/
 #include <iostream>
+#include <random>
+
+
+#define EXEPT_ROW 0
+using namespace std;
+
+random_device rd;
+mt19937 gen(rd());
+
+
+//----------------------------------------------------FUNCTIONS---------------------------------------------------------------------
+
+
+
+int Sign(int n) {
+    if (n % 2 == 0) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
+
+int Determine2by2(int m[4]) {
+    return m[0] * m[3] - m[1] * m[2];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+class Matrix {
+
+private:
+    int**       mat;
+    int         size = 3;
+
+
+private:
+
+
+    //todo : size == 4 일때 작동에 문제있음
+    //깊은복사를통해 같은 Matirx 구조체를 하나 더 만들어야함 
+    int* CreateMinorMatrix(int column) {
+        int MinorMatrix[4] = { 0, };
+        int c = 0;
+        for (auto i = 0; i < this->size; ++i) {
+            if (i == EXEPT_ROW) continue;
+            for (auto j = 0; j < this->size; ++j) {
+                if (j == column) continue;
+                MinorMatrix[c] = this->mat[i][j];
+                c += 1;
+
+            }
+        }
+
+        //cout << MinorMatrix[0] ;
+        //cout << MinorMatrix[1] << endl;
+        //cout << MinorMatrix[2] ;
+        //cout << MinorMatrix[3] << endl << endl << endl;
+
+
+
+
+
+        return MinorMatrix;
+    }
+
+
+
+
+public:
+    Matrix() {
+
+        uniform_int_distribution<> dist(0, 2);
+
+        
+
+        this->mat = new int*[4];
+        for (auto i = 0; i < 4; ++i) {
+
+            this->mat[i] = new int[4];
+            for (auto j = 0; j < 4; ++j) {
+                this->mat[i][j] = dist(gen);
+
+            }
+        }
+    }
+
+    Matrix(int z) {
+        this->mat = new int* [4];
+        for (auto i = 0; i < 4; ++i) {
+
+            this->mat[i] = new int[4];
+            for (auto j = 0; j < 4; ++j) {
+                this->mat[i][j] = 0;
+
+            }
+        }
+    }
+
+    ~Matrix() {
+
+        for (auto i = 0; i < 4; ++i) {
+            delete[] this->mat[i];
+        }
+        delete[] this->mat;
+
+
+    }
+
+
+    void print() {
+
+        for (auto i = 0; i < this->size; ++i) {
+            for (auto j = 0; j < this->size; ++j) {
+
+                cout << this->mat[i][j];
+
+            }
+
+            cout << endl;
+        }
+
+        cout << endl;    
+        cout << endl;    
+        cout << endl;    
+        cout << endl;
+
+    }
+
+
+    void reLocation() {
+        uniform_int_distribution<> dist(0, 2);
+        for (auto i = 0; i < this->size; ++i) {
+            for (auto j = 0; j < this->size; ++j) {
+                this->mat[i][j] = dist(gen);
+            }
+        }
+    }
+
+
+    // 1+1 1+2 1+3
+    // 1+0 1+1 1+2
+    int Determine() {
+        int result = 0;
+        for (auto i = 0; i < 3; ++i) {
+            result += Sign(i + 2 + EXEPT_ROW)*(this->mat[EXEPT_ROW][i])*(Determine2by2(this->CreateMinorMatrix(i)));
+        }
+        return result;
+    }
+
+    int TransDetermine() {
+
+        Matrix m(0);
+        
+
+
+        *this *= m;
+       
+        m.print();
+       
+
+        return m.Determine();
+    }
+
+
+    void Expantion() {
+        // 확장해도 행렬식은 결과적으로 같음 
+        // --> 4열을 선택할때, 여인수전개에서 결국 C_4_4에서만 여인수가 나오기때문(이때 여인수는 확장하기 전 행렬식과 같다 =>  (-1) ^ 4+4 = 1 이므로 부호도 같음 ). 
+        int d = this->Determine(); 
+        this->size = 4;
+
+        this->mat[0][3] = 0; this->mat[1][3] = 0; this->mat[2][3] = 0; this->mat[3][3] = 1;
+        this->mat[3][0] = 0; this->mat[3][1] = 0; this->mat[3][2] = 0; 
+
+
+        this->print();
+        cout << this->Determine() << endl;;
+
+
+
+    }
+
+    void operator*(int no) {
+
+        for (auto i = 0; i < this->size; ++i) {
+            for (auto j = 0; j < this->size; ++j) {
+
+                this->mat[i][j] = this->mat[i][j] * no;
+
+            }
+        }
+
+    }
+
+
+    
+
+    Matrix operator*(Matrix& other) {
+        int temp = 0;
+        Matrix result(0);
+
+        for (auto i = 0; i < this->size; ++i) {
+            for (auto j = 0; j < this->size; ++j) {
+                for (auto k = 0; k < this->size; ++k) {
+                    temp = this->mat[i][k] * other.mat[k][j];
+                    result.mat[i][j] += temp;
+
+                }
+
+
+
+            }
+        }
+
+        return result;
+    }
+
+   
+    
+    
+    Matrix operator+(Matrix& other) {
+        int temp = 0;
+        Matrix result(0);
+
+        for (auto i = 0; i < this->size; ++i) {
+            for (auto j = 0; j < this->size; ++j) {
+               
+                result.mat[i][j] = this->mat[i][j] + other.mat[i][j];
+
+            }
+        }
+
+        return result;
+    }
+
+    Matrix operator-(Matrix& other) {
+        int temp = 0;
+        Matrix result(0);
+
+        for (auto i = 0; i < this->size; ++i) {
+            for (auto j = 0; j < this->size; ++j) {
+
+                result.mat[i][j] = this->mat[i][j] - other.mat[i][j];
+
+            }
+        }
+
+        return result;
+    }
+
+
+
+
+
+
+    void operator*=(Matrix& other) {
+        for (auto i = 0; i < this->size; ++i) {
+            for (auto j = 0; j < this->size; ++j) {
+
+                other.mat[i][j] = this->mat[j][i];
+
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Matrix m1;
+    Matrix m2;
+
+    m1.print();
+   // m2.print();
+
+   //(m1 * m2).print();
+   //(m1 + m2).print();
+
+    //m1 * 2;
+
+   // m1.print();
+
+    cout << m1.Determine()          << endl;
+    cout << m1.TransDetermine()     << endl;
+
+    m1.Expantion();
+   
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
