4b78b3ecc857f876914658cc9c30c84a45956228
wewe231223 <wewe231223@kpu.ac.kr>
2023-09-07 17:14:31 +0900

    #2를위한 기초 파일 리드 초기화

--- P1/P1.cpp
+++ P1/P1.cpp
@@ -17,16 +17,74 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include "MatrixClass.h"
 using namespace std;
+
+
+
+class Text {
+private:
+	ifstream*	ifs			= nullptr;
+	char**		readbuffer	= nullptr;
+public:
+	Text(const char* path) {
+		this->ifs = new ifstream(path,ios::in);
+		if (this->ifs->fail()) {
+			throw -1;
+		}
+
+		this->readbuffer = new char* [10];
+		for (auto i = 0; i < 10; ++i) {
+			this->readbuffer[i] = new char[256];
+		}
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+};
+
+
 
 
 
 
 int main()
 {
+	try{
+		Text* T = new Text("TEST.txt");
+	}
+	catch (int exeption)
+	{
+		if (exeption == -1) {
+			cout << "File read Failed" << endl;
+		}
+	}
+
+
 	
 
+
+
+
+
+
+
+
+
+
+
+	
+	return 1;
 	//TestProblem_1();
 
 
